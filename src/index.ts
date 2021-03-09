a73af2d9a004598564e18ea437663bb1596a6c07
yamyam <yamyam@pentabreed.com>
2021-03-09 18:41:15 +0900

    create block 예제 완성

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-
+import * as CryptoJs from "crypto-js";
 
 class Block {
   public index: number;
@@ -8,6 +8,13 @@ class Block {
   public timestamp: number;
 
   
+  static calculateBlockHash = (
+    index: number, 
+    previousHash:string, 
+    timestamp:number, 
+    data: string
+  ) : string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+
 
   constructor(
     index: number,
@@ -26,7 +33,34 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "2020202", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
-console.log(blockchain)
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1]
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+
+const createNewBlock = (data:string): Block => {
+  const previosBlock: Block = getLatestBlock();
+  const newIndex: number = previosBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex, 
+    previosBlock.hash, 
+    newTimestamp, 
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex, 
+    newHash, 
+    previosBlock.hash, 
+    data, 
+    newTimestamp
+  )
+  return newBlock;
+}
+
+console.log(createNewBlock('hello'), createNewBlock('bye bye'));
 
 export {};
